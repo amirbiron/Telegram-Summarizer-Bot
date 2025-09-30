e3e040ce40dfc282df04a64bdd2bade553a95b45
amir haim <amirbiron@gmail.com>
2025-09-30 12:14:25 +0300

    Initialize utils package with logging setup
    Add docstring and exports for utilities package

---
+++ utils/__init__.py
@@ -0,0 +1,14 @@
+"""
+Utilities package for helper functions and tools.
+
+This package provides:
+- Logging configuration
+- Helper functions
+- Constants
+"""
+
+from utils.logger import setup_logging
+
+__all__ = [
+    "setup_logging",
+]
